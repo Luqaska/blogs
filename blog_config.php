cf79163d7decfc32bec2c7e1684bc5db396c5cdc
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-08 15:36:43 -0300

    Create blog_config.php

---
+++ blog_config.php
@@ -0,0 +1,14 @@
+<?php // Settings
+$title="Blog de Lucas"; // Obligatory!!! Put the name of your blog here
+$description=""; // A description
+$website="https://pages.7ort.rf.gd/lucas/"; // Include protocol (ex. https://example.com)
+$custom_css=""; // Put the url of your custom css
+$utterances=''; // Get it on https://utteranc.es. Please use the "Issue title contains page URL"
+// Posts
+$posts=[
+    [
+        "9/4/2021",
+        "Lol",
+        "First post\nNice"
+    ]
+]; ?>
