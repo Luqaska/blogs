6d2615a3c5a56f305f17239e04c5bd540ec834f6
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-08 15:36:53 -0300

    Update blog_config.php

--- blog_config.php
+++ blog_config.php
@@ -1,7 +1,7 @@
 <?php // Settings
 $title="Blog de Lucas"; // Obligatory!!! Put the name of your blog here
 $description=""; // A description
-$website="https://pages.7ort.rf.gd/lucas/"; // Include protocol (ex. https://example.com)
+$website="https://pg.7ort.rf.gd/lucas/"; // Include protocol (ex. https://example.com)
 $custom_css=""; // Put the url of your custom css
 $utterances=''; // Get it on https://utteranc.es. Please use the "Issue title contains page URL"
 // Posts
