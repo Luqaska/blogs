8e20f9b9ad58e66c531ba36d3f47084b848531a8
Lucas <83518257+lu700@users.noreply.github.com>
2021-07-08 20:12:29 -0300

    Create frifoss.php

---
+++ frifoss.php
@@ -0,0 +1,10 @@
+<?php // Settings
+$title="Frifoss"; // Obligatory!!! Put the name of your blog here
+$description="Noticias sobre Frifoss"; // A description
+$website="https://frifoss.7ort.rf.gd"; // Include protocol (ex. https://example.com)
+$custom_css=""; // Put the url of your custom css
+$utterances=''; // Get it on https://utteranc.es. Please use the "Issue title contains page URL"
+// Posts
+$posts=[
+    ["8/7/2021","¡¡¡Nuevo canal de noticias!!!","Inaguramos este canal de noticias para documentar novedades y cosas de Frifoss :D"]
+]; ?>
