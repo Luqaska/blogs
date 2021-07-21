dfa88fd0fd252e9ad5442c8ba3fe020335ec6d3f
Lucas <83518257+lucotoh@users.noreply.github.com>
2021-07-21 20:08:33 -0300

    Cagó otra vez... :(

--- frifoss.php
+++ frifoss.php
@@ -6,5 +6,5 @@ $utterances='';
 $posts=[
     ["8/7/2021","¡¡¡Nuevo canal de noticias!!!","Inaguramos este canal de noticias para documentar novedades y cosas de Frifoss :D"],
     ["20/7/2021","Cambio de URL y de dueño","¡Hola!\nEsta web ahora esta siendo desarrollada solo por Lucas Tjor, el desarrollador original. Por esto, ¡Inaguramos una nueva URL!\n<a href='https://frifoss.repl.co'>frifoss.repl.co</a>\nEsto no va a significar ningún cambio negativo, ya que los juegos van a seguir estando disponibles y la web se vá a continuar desarrollando."],
-    ["21/7/2021","Nuevo favicon y eliminación de el nombre de 7ort","¡Hola!\nComo sabrán, ahora no este proyecto no está asociado con 7ort, así que decidí cambiar los logos de 7ort, ¡Por uno original!\n<img width="50%"src='https://frifoss.repl.co/cdn/imgs/icon.png' />"]
+    ["21/7/2021","Nuevo favicon y eliminación de el nombre de 7ort","¡Hola!\nComo sabrán, ahora no este proyecto no está asociado con 7ort, así que decidí cambiar los logos de 7ort, ¡Por uno original!\n<img width='50%' src='https://frifoss.repl.co/cdn/imgs/icon.png' />"]
 ]; ?>
