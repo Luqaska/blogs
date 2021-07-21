db4a08fd9256e81fedfabdf4c89944c0e755a1ab
Lucas <83518257+lucotoh@users.noreply.github.com>
2021-07-21 19:22:24 -0300

    Nueva URL goes brrr

--- frifoss.php
+++ frifoss.php
@@ -1,7 +1,7 @@
 <?php $title="Frifoss";
 $description="Noticias sobre Frifoss";
-$website="https://frifoss.7ort.rf.gd";
-$custom_css="https://frifoss.7ort.rf.gd/css/styles.css";
+$website="https://frifoss.repl.co";
+$custom_css="https://frifoss.repl.co/css/styles.css";
 $utterances='';
 $posts=[
     ["8/7/2021","¡¡¡Nuevo canal de noticias!!!","Inaguramos este canal de noticias para documentar novedades y cosas de Frifoss :D"],
